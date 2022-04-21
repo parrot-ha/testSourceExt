9dc5b4ee60ed223b5d57831fd564b56468bf4670
psittaciform <90155287+psittaciform@users.noreply.github.com>
2022-04-21 12:23:04 -0500

    Create AutomationApp.groovy

---
+++ AutomationApp.groovy
@@ -0,0 +1,42 @@
+definition(
+    name: "My First SmartApp",
+    namespace: "mygithubusername",
+    author: "Peter Gregory",
+    description: "This is my first SmartApp. Woot!",
+    category: "My Apps",
+    iconUrl: "https://s3.amazonaws.com/smartapp-icons/Convenience/Cat-Convenience.png",
+    iconX2Url: "https://s3.amazonaws.com/smartapp-icons/Convenience/Cat-Convenience@2x.png",
+    iconX3Url: "https://s3.amazonaws.com/smartapp-icons/Convenience/Cat-Convenience@2x.png")
+
+preferences {
+    section("Turn on when motion detected:") {
+        input "themotion", "capability.motionSensor", required: true, title: "Where?"
+    }
+    section("Turn on this light") {
+        input "theswitch", "capability.switch", required: true
+    }
+}
+
+def installed() {
+    log.debug "Installed with settings: ${settings}"
+    initialize()
+}
+
+def updated() {
+    log.debug "Updated with settings: ${settings}"
+    unsubscribe()
+    initialize()
+}
+
+def initialize() {
+    subscribe(themotion, "motion.active", motionDetectedHandler)
+}
+
+def motionDetectedHandler(evt) {
+    log.debug "motionDetectedHandler called: $evt"
+}
+
+def motionDetectedHandler(evt) {
+    log.debug "motionDetectedHandler called: $evt"
+    theswitch.on()
+}
